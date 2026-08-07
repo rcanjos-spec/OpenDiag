6f9981fd5851b44747af802f18e50f6ae06cfee9
Rogerio Anjos <rc_anjos@msn.com>
2026-08-07 10:30:12 -0300

    test(isotp): improve consecutive frame coverage

--- tests/test_isotp_consecutive_frame.py
+++ tests/test_isotp_consecutive_frame.py
@@ -5,8 +5,26 @@ from opendiag.protocols.isotp import (
 
 
 def test_parse_consecutive_frame() -> None:
-    frame = ISOTPFrame.from_can_data(b"\x21\xaa\xbb\xcc\xdd\xee\xff\x11")
+    # Arrange
+    data = b"\x21\xaa\xbb\xcc\xdd\xee\xff\x11"
 
+    # Act
+    frame = ISOTPFrame.from_can_data(data)
+
+    # Assert
     assert frame.frame_type is FrameType.CONSECUTIVE
+    assert frame.sequence_number == 1
+    assert frame.payload == b"\xaa\xbb\xcc\xdd\xee\xff\x11"
 
-    assert frame.payload == (b"\xaa\xbb\xcc\xdd\xee\xff\x11")
+
+def test_parse_consecutive_frame_sequence_number_15() -> None:
+    # Arrange
+    data = b"\x2f\xaa\xbb"
+
+    # Act
+    frame = ISOTPFrame.from_can_data(data)
+
+    # Assert
+    assert frame.frame_type is FrameType.CONSECUTIVE
+    assert frame.sequence_number == 15
+    assert frame.payload == b"\xaa\xbb"
